d99c49602b0bf521406e1c05bcd4dceff6c3df22
Matthew Casperson <matthewcasperson@gmail.com>
2023-06-06 14:28:58 +1000

    Started work on a sample log file scanner

---
+++ python/octopub-log-scanner/octopub-log-scanner.py
@@ -0,0 +1,30 @@
+import sys
+import re
+
+
+class Hint:
+    def __init__(self, error, hint):
+        self.error = error
+        self.hint = hint
+
+
+hints = [
+    Hint("java.nio.file.FileSystemException:.*?Read-only file system",
+         "The Kubernetes.Application.ReadOnlyFileSystem variable may need to be set to false")
+]
+
+if len(sys.argv) < 2:
+    print("The first argument must be the log file to parse.")
+    sys.exit(1)
+
+
+def check_log(log):
+    with open(log) as f:
+        for line in f:
+            for hint in hints:
+                if re.search(hint.error, line) is not None:
+                    print("Possible error: " + line)
+                    print(hint.hint)
+
+
+check_log(sys.argv[1])
