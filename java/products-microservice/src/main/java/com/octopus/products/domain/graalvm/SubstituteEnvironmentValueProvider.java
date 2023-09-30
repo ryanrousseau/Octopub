7a890ac028e9601d83a599b6bf39aacad381fcd0
Matthew Casperson <matthewcasperson@gmail.com>
2023-09-30 13:33:36 +1000

    Fixing up windows builds

---
+++ java/products-microservice/src/main/java/com/octopus/products/domain/graalvm/SubstituteEnvironmentValueProvider.java
@@ -0,0 +1,24 @@
+package com.octopus.products.domain.graalvm;
+
+import com.oracle.svm.core.annotate.Delete;
+import com.oracle.svm.core.annotate.Substitute;
+import com.oracle.svm.core.annotate.TargetClass;
+
+import java.util.Map;
+
+/**
+ * Reimplements the fix from https://github.com/quarkusio/quarkus/pull/25960
+ * See https://github.com/quarkusio/quarkus/issues/33030
+ */
+@TargetClass(className = "liquibase.configuration.pro.EnvironmentValueProvider")
+final class SubstituteEnvironmentValueProvider {
+
+  @Delete
+  private Map<String, String> environment;
+
+  @Substitute
+  protected Map<?, ?> getMap() {
+    return System.getenv();
+  }
+
+}
