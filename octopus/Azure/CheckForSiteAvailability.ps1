733d7c72bf7ce01c4fc42587217df6a80697aabb
twerthi <twerthi@hotmail.com>
2025-05-12 08:50:08 -0700

    Adding script file to check for Azure Site name availability.

---
+++ octopus/Azure/CheckForSiteAvailability.ps1
@@ -0,0 +1,31 @@
+# Configure payload
+$jsonPayload = @{
+  "name" = "#{Project.Azure.Function.Octopub.Products.Name}"
+  "type" = "Microsoft.Web/sites"
+  "isFQDN" = $false
+}
+
+# Construct header
+$headers = @{
+  Authorization = "Bearer #{Octopus.Action[Get Azure Access Token].Output.AzureToken}"
+  ContentType = "application/json"
+}
+
+# Fix ANSI Color on PWSH Core issues when displaying objects
+if ($PSEdition -eq "Core") {
+    $PSStyle.OutputRendering = "PlainText"
+}
+
+$response = (Invoke-RestMethod -Method Post -Uri "https://management.azure.com/subscriptions/#{Project.Azure.Account.SubscriptionNumber}/providers/Microsoft.Web/checknameavailability?api-version=2024-04-01" -Body $jsonPayload -Headers $headers)
+
+# Set output variable
+Set-OctopusVariable -Name NameAvailable -Value $response.nameAvailable
+
+if ($response.nameAvailable -ne $True)
+{
+  Write-Highlight "The Function Name of #{Project.Azure.Function.Octopub.Products.Name} is already in use, please change the Project Variable Project.Azure.Function.Octopub.Products.Name to something else."
+}
+else
+{
+  Write-Host "Function name available!"
+}
