c08315b6f27f78a493aa40227277dee2a30bbb8e
Matthew Casperson <matthewcasperson@gmail.com>
2025-07-09 18:31:25 +1000

    Add quiet flag again

--- octopus/DockerImageSbomScan.ps1
+++ octopus/DockerImageSbomScan.ps1
@@ -57,7 +57,7 @@ foreach ($file in $bomFiles) {
 
     # Generate the report, capturing the output
     try {
-        $OUTPUT = ./trivy sbom "$($file.FullName)"
+        $OUTPUT = ./trivy sbom -q "$($file.FullName)"
         $exitCode = $LASTEXITCODE
     }
     catch {
@@ -66,7 +66,7 @@ foreach ($file in $bomFiles) {
     }
 
     # Run again to generate the JSON output
-    ./trivy sbom -f json -o depscan-bom.json "$($file.FullName)"
+    ./trivy sbom -q -f json -o depscan-bom.json "$($file.FullName)"
 
     # Octopus Deploy artifact
     New-OctopusArtifact "$PWD/depscan-bom.json"
